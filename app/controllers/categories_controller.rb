ca7441e20ab78b8d7f0c482b7bd92a65475db005
Badichan Polina <badichan@mail.ru>
2018-01-08 21:39:43 +0100

    error

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,9 +12,33 @@ class CategoriesController < ApplicationController
 
   def new
     @category = Category.new
-    @category.parent_category = Category.find(params[:id]) unless params[:id].nil?
+    @category.parent_id = Category.find(params[:id]).try(:id) unless params[:id].nil?
   end
 
-  
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:text)
+  end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -20,6 +20,4 @@ class Category < ApplicationRecord
   has_many :subcategories, class_name: "Category", foreign_key: "parent_id", dependent: :destroy
   belongs_to :parent_category, class_name: "Category", optional: true
 
-  scope :parent_category, -> { where(parent_id: self.parent_id) }
-
 end
