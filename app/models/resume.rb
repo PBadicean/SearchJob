645894376764cc8fe73e6cc13b148af1764edf63
Badichan Polina <badichan@mail.ru>
2018-01-12 23:01:48 +0100

    fix error resume

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -2,21 +2,19 @@
 #
 # Table name: resumes
 #
-#  id            :integer          not null, primary key
-#  position      :string           not null
-#  salary        :integer          not null
-#  user_id       :integer          not null
-#  category_id   :integer          not null
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  about_me      :text
-#  skills        :string
-#  experience_id :integer
+#  id               :integer          not null, primary key
+#  position         :string           not null
+#  salary           :integer          not null
+#  user_id          :integer          not null
+#  about_me         :integer          not null
+#  experience_id_id :integer          not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
 #
 # Indexes
 #
-#  index_resumes_on_category_id  (category_id)
-#  index_resumes_on_user_id      (user_id)
+#  index_resumes_on_experience_id_id  (experience_id_id)
+#  index_resumes_on_user_id           (user_id)
 #
 
 class Resume < ApplicationRecord
@@ -24,7 +22,7 @@ class Resume < ApplicationRecord
   belongs_to :user
   belongs_to :category
   belongs_to :experience
-  accepts_nested_attributes_for :experience
 
+  accepts_nested_attributes_for :experience
 
 end
