25b0192f41e904f3c5ddf187dc646332ea15e11c
Badichan Polina <badichan@mail.ru>
2018-01-16 16:14:31 +0100

    Tags added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery3
+//= require jquery-ui
 //= require cocoon
 //= require popper
 //= require bootstrap
---
+++ db/migrate/20180116115447_add_tags_to_resume.rb
@@ -0,0 +1,5 @@
+class AddTagsToResume < ActiveRecord::Migration[5.1]
+  def change
+    add_column :resumes, :tags, :string
+  end
+end
