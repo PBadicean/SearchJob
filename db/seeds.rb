35dfb86c09326516523cf11b43f8e2f545e1c93f
Badichan Polina <badichan@mail.ru>
2018-02-08 11:49:11 +0100

    fix for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,13 +26,13 @@ end
 
 Category.create do |subcategory|
   subcategory.name = 'Programming'
-  subcategory.parent_id=Category.first.try(:id)
+  subcategory.parent_id = Category.first.try(:id)
 end
 
 7.times do |i|
   Resume.create do |resume|
     resume.position = "RoR Programmer #{i}"
-    resume.salary = '35000'
+    resume.salary = 35000
     resume.user = Candidate.first
     resume.category = Category.where(name: 'Programming').first
     resume.tags = "Ruby, Rails, Js, JQuery, Python, HTML, CSS, SCSS"
@@ -41,32 +41,32 @@ end
 end
 
 Experience.create do |experience|
-  experience.date_start='04.06.2015'
-  experience.date_end='01.07.2016'
-  experience.position='Junior'
-  experience.company='Audi'
-  experience.resume=Resume.first
-  experience.duty='The official online destination for Call of Duty t-shirts, hoodies, jackets, hats, collectibles, posters, toys, and other exclusive apparel and accessories.'
+  experience.date_start = '04.06.2015'
+  experience.date_end = '01.07.2016'
+  experience.position = 'Junior'
+  experience.company = 'Audi'
+  experience.resume = Resume.first
+  experience.duty = 'The official online destination for Call of Duty t-shirts, hoodies, jackets, hats, collectibles, posters, toys, and other exclusive apparel and accessories.'
 end
 
 Experience.create do |experience|
-  experience.date_start='04.06.2012'
-  experience.date_end='01.07.2014'
-  experience.position='TeamLead'
-  experience.company='Mail.ru'
-  experience.resume=Resume.first
-  experience.duty='The hoodies, jackets, hats, collectibles, posters, toys, and other official online destination for Call of Duty t-shirts,exclusive apparel and accessories.'
+  experience.date_start = '04.06.2012'
+  experience.date_end = '01.07.2014'
+  experience.position = 'TeamLead'
+  experience.company = 'Mail.ru'
+  experience.resume = Resume.first
+  experience.duty = 'The hoodies, jackets, hats, collectibles, posters, toys, and other official online destination for Call of Duty t-shirts,exclusive apparel and accessories.'
 end
 
 Vacancy.create do |vacancy|
-  vacancy.name="Ruby Programmer"
-  vacancy.salary_min='25000'
-  vacancy.salary_max='50000'
-  vacancy.discription='The hoodies, jackets, hats, collectibles, posters, toys, and other official online destination for Call of Duty t-shirts,exclusive apparel and accessories'
-  vacancy.experience=1
-  vacancy.employer=Employer.first
-  vacancy.place_id='1234'
-  vacancy.schedule=1
-  vacancy.category_id=Category.first
-  vacancy.key_skills='Ruby, Programming, Java'
+  vacancy.name = "Ruby Programmer"
+  vacancy.salary_min = 25000
+  vacancy.salary_max = 50000
+  vacancy.schedule = 1
+  vacancy.experience = 1
+  vacancy.employer = Employer.first
+  vacancy.category_id = Category.first
+  vacancy.discription = 'The hoodies, jackets, hats, collectibles, posters, toys, and other official online destination for Call of Duty t-shirts,exclusive apparel and accessories'
+  vacancy.place_id = 'ChIJ53USP0nBhkcRjQ50xhPN_zw'
+  vacancy.key_skills = 'Ruby, Programming, Java'
 end
